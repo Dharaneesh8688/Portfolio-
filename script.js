ca64ed077d4170795ddcdc8c34a16e6d3d51cd0b
Dharaneesh8688 <dharaneesh8688@gmail.com>
2024-01-07 16:43:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*$(document).ready(function(){
+    $("#fade").animate({
+        height:'100%',
+        left:'100%',
+        width:'100%',
+         },2500);
+         $("#about").animate({
+            height:'100%',
+            left:'100%',
+            width:'100%',
+             },2500);
+});*/
+$(document).ready(function() {
+    $("#fade").animate({
+        height:'100%',
+        left:'100%',
+        width:'100%',
+         },2500);
+
+});
