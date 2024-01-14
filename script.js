8058d05cc5aff2e640d81ba42f52b8bf76990925
dharaneesh8688 <dharaneesh8688@gmail>
2024-01-14 09:38:46 +0530

    update

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-/*$(document).ready(function(){
-    $("#fade").animate({
-        height:'100%',
-        left:'100%',
-        width:'100%',
-         },2500);
-         $("#about").animate({
-            height:'100%',
-            left:'100%',
-            width:'100%',
-             },2500);
-});*/
-$(document).ready(function() {
-    $("#fade").animate({
-        height:'100%',
-        left:'100%',
-        width:'100%',
-         },2500);
 
-});
+$(document).ready(function() {
+   
+    $('#fade').hide();
+    $('.card').hide();
+    $('#about').hide();
+    $('#skills').hide();
+   
+         $(document).ready(function() {
+            $('#fade').slideDown(3000);
+           
+         });
+
+
+         $(window).scroll(function(){
+          
+            $('#about').slideDown(3000);
+        });
+        $(window).scroll(function() {
+            var targetPosition = $('#skills').offset().top;
+            var scrollPosition = $(window).scrollTop();
+            var offset = 10;
+            if (scrollPosition = targetPosition - offset) {
+                $('#skills').slideDown(3000);
+                $
+            }
+        });
+        $(window).scroll(function() {
+            var targetPosition = $('#about').offset().top;
+            var scrollPosition = $(window).scrollTop();
+            var offset = 10;
+            if (scrollPosition = targetPosition - offset) {
+                $('.card').slideDown(5000);
+                $
+            }
+        });
+      
+    });
