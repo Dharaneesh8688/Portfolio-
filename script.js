301db91283d4a31308b30ff160a9f8c6a8ff7c87
Dharaneeesh8688 <dharaneesh8688@gmail.com>
2024-01-15 12:14:00 +0530

    update

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
 
 $(document).ready(function() {
    
-    $('#fade').hide();
-    $('.card').hide();
-    $('#about').hide();
-    $('#skills').hide();
-   
-         $(document).ready(function() {
-            $('#fade').slideDown(3000);
-           
-         });
-
-
-         $(window).scroll(function(){
-          
-            $('#about').slideDown(3000);
-        });
-        $(window).scroll(function() {
-            var targetPosition = $('#skills').offset().top;
-            var scrollPosition = $(window).scrollTop();
-            var offset = 10;
-            if (scrollPosition = targetPosition - offset) {
-                $('#skills').slideDown(3000);
-                $
-            }
-        });
-        $(window).scroll(function() {
-            var targetPosition = $('#about').offset().top;
-            var scrollPosition = $(window).scrollTop();
-            var offset = 10;
-            if (scrollPosition = targetPosition - offset) {
-                $('.card').slideDown(5000);
-                $
-            }
-        });
+    AOS.init();
       
     });
